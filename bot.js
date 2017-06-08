afada745f78ca0a63653ff7c37a1f09216b61252
antoinelelivec <antoine.le-livec@isep.fr>
2017-06-09 00:27:02 +0200

    changements du code de base Spotify

--- bot.js
+++ bot.js
@@ -23,7 +23,70 @@ client.on('message', msg => {
   } else if (msg.content.startsWith('!twitter')) {
 
   } else if (msg.content.startsWith('!spotify')) {
-
+    var searchItem = msg.content.substr(9, msg.content.length)
+    var SpotifyWebApi = require('spotify-web-api-node')
+    var spotifyApi = new SpotifyWebApi({
+      clientId: '34310d56bc4e479585c09dadc3877a88',
+      clientSecret: 'ced4ae31372f4687906a5f069a325b55'
+    })
+    spotifyApi.clientCredentialsGrant()
+    .then(function (data) {
+      spotifyApi.setAccessToken(data.body['access_token'])
+      var result = spotifyApi.search(searchItem, ['track', 'artist', 'album'], function (err, data) {
+        if (err) {
+          console.error('Something went wrong', err.message)
+        }
+      })
+      result.then(function (result) {
+        var i = 0
+        var tracksIndex = 0
+        var artistsIndex = 0
+        var albumsIndex = 0
+        var resultArray = []
+        while (i < 3) {
+          if (result.body.tracks.total !== 0) {
+            resultArray.push({'type': 'track', 'index': tracksIndex, 'name': result.body.tracks.items[tracksIndex].name})
+            tracksIndex++
+            i++
+          } if (i < 3) {
+            if (result.body.artists.total !== 0) {
+              resultArray.push({'type': 'artist', 'index': artistsIndex, 'name': result.body.artists.items[artistsIndex].name})
+              artistsIndex++
+              i++
+            }
+          } if (i < 3) {
+            if (result.body.albums.total !== 0) {
+              resultArray.push({'type': 'album', 'index': albumsIndex, 'name': result.body.albums.items[albumsIndex].name})
+              albumsIndex++
+              i++
+            }
+          }
+        }
+        console.log(resultArray)
+        for (i = 0; i < resultArray.length; i++) {
+          if (resultArray[i].type === 'track') {
+            if (resultArray[i].index === 0) {
+              msg.channel.send('Titre(s) trouvé(s) :')
+            }
+            msg.channel.send(resultArray[i].name)
+          }
+        } for (i = 0; i < resultArray.length; i++) {
+          if (resultArray[i].type === 'artist') {
+            if (resultArray[i].index === 0) {
+              msg.channel.send('Artiste(s) trouvé(s) :')
+            }
+            msg.channel.send(resultArray[i].name)
+          }
+        } for (i = 0; i < resultArray.length; i++) {
+          if (resultArray[i].type === 'album') {
+            if (resultArray[i].index === 0) {
+              msg.channel.send('Album(s) trouvé(s) :')
+            }
+            msg.channel.send(resultArray[i].name)
+          }
+        }
+      })
+    })
   } else if (msg.content.startsWith('!youtube')) {
     var youSearch = ''
     if (msg.content === '!youtube hello') {
