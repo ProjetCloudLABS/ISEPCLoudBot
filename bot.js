e7e4cbef838522434c2efe5a1fc81731e6cf24d3
ikki75 <hnouis@gmail.com>
2017-06-08 09:52:53 +0200

    youtubeTest

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+var getreq = require('request')
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -21,7 +23,25 @@ client.on('message', msg => {
   } else if (msg.content.startsWith('!spotify')) {
 
   } else if (msg.content.startsWith('!youtube')) {
-
+    var youSearch = ''
+    if (msg.content === '!youtube hello') {
+      msg.channel.sendMessage('youtube hello to you too')
+    } else if (msg.content.startsWith('!youtubeS')) {
+      youSearch = msg.content.substr(9, msg.content.length)
+      getreq({ url: 'https://www.googleapis.com/youtube/v3/search?q=' + youSearch + '&part=snippet&key=AIzaSyAtGurwN6GfssCwVSHXzPRbA4ofxYM5FRo', json: true }, function (error, response, dataYoutube) {
+        if (error) {
+          // oh no !!!
+          msg.channel.sendMessage(' connection error ')
+        } else {
+          if (dataYoutube.items && dataYoutube.items.length > 2) {
+            msg.channel.sendMessage('Les trois premières vidéos sont ' + dataYoutube.items[0].snippet.title +
+             ' ; ' + dataYoutube.items[1].snippet.title + ' et ' + dataYoutube.items[2].snippet.title + '.')
+          } else {
+            msg.channel.sendMessage(youSearch + ' Pas de résultats.')
+          }
+        }
+      })
+    }
   } else if (msg.content.startsWith('!translate')) {
 
   } else if (msg.content.startsWith('!weather')) {
