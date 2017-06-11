6c8c7cd2620d49cb673c6ab35fe9a0d7b367c951
antoinelelivec <antoine.le-livec@isep.fr>
2017-06-11 22:54:39 +0200

    correction tests unitaires (non validés) pour Travis

--- tests/testsSpotify.js
+++ tests/testsSpotify.js
@@ -5,10 +5,23 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: 'ced4ae31372f4687906a5f069a325b55'
 })
 
-
 spotifyApi.clientCredentialsGrant()
+  .then(function (data) {
+    spotifyApi.setAccessToken(data.body['access_token'])
+  })
 
-.then(function (data) {
-  spotifyApi.setAccessToken(data.body['access_token'])
-  console.log('autorisation')
+test('Example test', t => {
+  spotifyApi.search('muse', ['track', 'artist', 'album'], function (err, data) {
+    if (err) {
+      console.error('Something went wrong', err.message)
+    }
+  })
+  .catch((error) => {
+    t.fail()
+    throw error
+  })
+  .then((res) => {
+    console.log(res.response.statusCode)
+    t.is(res.response.statusCode, 200)
+  })
 })
